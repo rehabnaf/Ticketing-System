d722319d1b6b70978e6c455b7c6e30573e5219a0
Rehab <88044583+rehabnaf@users.noreply.github.com>
2024-11-12 21:58:42 +0530

    Update the validation method name to make it more meaningful

--- Configuration.java
+++ Configuration.java
@@ -8,7 +8,7 @@ public class Configuration {
     private int customerRetrievalRate;
     private int  maximumTicketCapacity;
 
-    private static int validation(Scanner input, String message) {
+    private static int validateInput(Scanner input, String message) {
       boolean continueUserInput = true;
       int value = 0;
         do {
@@ -34,10 +34,10 @@ public class Configuration {
 
     public void getUserInput(){
         Scanner input = new Scanner(System.in);
-        this.totalNumOfTickets = validation(input,"Enter the total number of tickets: ");
-        this.ticketReleaseRate = validation(input,"Enter the ticket release rate: ");
-        this.customerRetrievalRate = validation(input,"Enter the customer retrieval rate: ");
-        this.maximumTicketCapacity = validation(input,"Enter the maximum ticket capacity: ");
+        this.totalNumOfTickets = validateInput(input,"Enter the total number of tickets: ");
+        this.ticketReleaseRate = validateInput(input,"Enter the ticket release rate: ");
+        this.customerRetrievalRate = validateInput(input,"Enter the customer retrieval rate: ");
+        this.maximumTicketCapacity = validateInput(input,"Enter the maximum ticket capacity: ");
     }
 
     public int getTotalNumOfTickets(){
