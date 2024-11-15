74b318f315d08aa922e86da2173b8925f0c87dc2
Rehab <88044583+rehabnaf@users.noreply.github.com>
2024-11-15 21:57:45 +0530

    Add the Customer class to remove tickets from the ticket pool

---
+++ src/Customer.java
@@ -0,0 +1,31 @@
+package src;
+
+public class Customer implements Runnable {
+    private int customerID;
+    private int retrievalInterval;
+    private TicketPool ticketPool;
+    private Configuration configObj;
+    private int customerRetrievalRate;
+
+    public Customer(int customerID,TicketPool ticketpool,Configuration configObj){
+        this.customerID = customerID;
+        this.retrievalInterval = 4000;
+        this.ticketPool = ticketpool;
+        this.customerRetrievalRate = configObj.getCustomerRetrievalRate();
+    }
+
+    public void run(){
+        while(true){
+            for (int i = 0; i < customerRetrievalRate; ++i){
+                ticketPool.removeTicket();
+            }
+            try {
+                Thread.sleep(retrievalInterval);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+}
