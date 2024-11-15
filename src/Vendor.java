26cf53d8d63dc7590cb3579a260adbcc3c98fcb7
Rehab <88044583+rehabnaf@users.noreply.github.com>
2024-11-15 20:37:52 +0530

    Add the Vendor class to release tickets to the ticket pool

---
+++ src/Vendor.java
@@ -0,0 +1,29 @@
+package src;
+
+public class Vendor implements Runnable {
+    private Configuration configObj;
+    private int vendorId;
+    private int ticketsPerRelease;
+    private int releaseInterval;
+    private TicketPool ticketPool;
+
+    public Vendor(TicketPool ticketPool,int vendorId,Configuration configObj){
+        this.ticketPool = ticketPool;
+        this.vendorId = vendorId;
+        this.ticketsPerRelease = configObj.getTicketReleaseRate();
+        this.releaseInterval = 3500; // Thread sleep duration
+    }
+    public void run(){
+            while (true){
+                // Making a vendor release tickets in batches according to the configuration (ticketsPerRelease)
+                for (int i = 0; i < ticketsPerRelease;++i){
+                    ticketPool.addTickets(new Ticket());
+                }
+                try {
+                    Thread.sleep(releaseInterval);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+    }
+}
